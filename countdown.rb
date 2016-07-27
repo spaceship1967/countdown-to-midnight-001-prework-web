079bbc41dea950aa1512af851cb5eb9d974e8643
Michael Wiggins <michael.wiggins@me.com>
2016-07-27 16:33:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+  countdown_output = 0
+  puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 10
+end
+
+def countdown_with_sleep
 end
